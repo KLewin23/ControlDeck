f0f6432584de1c585b4297b4ddcd3e71fb4414ff
Kieran Lewin <afrenchrussian@gmail.com>
2020-08-18 05:15:21 +0100

    add profiles, add devices, mod devices, mod profiles,bug fixes
    Former-commit-id: 155a85b74cb12ab5ca682a0ef2ff008756067518

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -2,7 +2,12 @@ import {createStore} from "redux";
 import MainReducer from "./Reducers/mainReducer";
 
 export enum ActionType {
-    ADD_DEVICE
+    ADD_PROFILE,
+    MODIFY_PROFILE,
+    MODIFY_BUTTON,
+    SET_PROFILE_PAGE_STATE,
+    ADD_DEVICE,
+    MODIFY_DEVICE
 }
 
 //const rootReducer = combineReducers({userReducer: UserInfoReducer,buildScreenReducer: BuildScreenReducer})
